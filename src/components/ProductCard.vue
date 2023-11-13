12b339374907b390287c099524478fd7064fdb40
“Marco <marco.pianaroli96@gmail.com>
2023-11-13 22:24:56 +0100

    adds card info

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -7,11 +7,12 @@ defineProps<{
 </script>
 
 <template>
-  <div class=" p-2 h-80 rounded-lg m-2 bg-red-500">
-    <p>{{ product.title }}</p>
-    <img :src="product.images[0]">
-    <p>{{ product.price }}</p>
-  </div>
+  <article class=" w-86 flex m-2 h-full flex-col items-center justify-between rounded-lg bg-blue-500  shadow transition-transform  duration-75 hover:scale-[1.01] hover:shadow-lg card-container  ">
+    <h3 class="text-center text-xl">{{ product.title }}</h3>
+    <figure class="h-64 bg-red-500"><img class="object-contain h-full w-full" :src="product.thumbnail"> </figure>
+    <span>${{ product.price }}</span>
+    <button class="bg-blue-600 hover:bg-blue-400 rounded-full p-2">Add to Cart</button>
+  </article>
 </template>
 
 <style>
