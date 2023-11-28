a856406707400e819e08d39cb88bfe78030e5744
“Marco <marco.pianaroli96@gmail.com>
2023-11-28 19:49:11 +0100

    adds image picker on pageproductdetails

--- src/pages/PageProductDetails.vue
+++ src/pages/PageProductDetails.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { defineProps, ref } from 'vue';
 import type { Product } from '../../types';
 import { useStore } from '../composable/useStore';
 
@@ -7,13 +8,17 @@ const props = defineProps<{
 }>()
 
 const { handleIncrement } = useStore()
+const activeImage = ref<string>(props.productData.images[0])
+const setActiveImage = (image: string) => {
+  activeImage.value = image
+}
 </script>
 
 <template>
   <div class="grid md:grid-cols-2 gap-6 lg:gap-12 items-start max-w-6xl px-4 mx-auto py-6">
     <div class="grid gap-4">
-      <img :src="productData.images[0]" alt="Product Image" width="600" height="600" class="aspect-square object-cover border border-zinc-200 w-full rounded-lg overflow-hidden dark:border-zinc-800"><div class="flex gap-4">
-        <img v-for="(image, index) in productData.images" :key="index" :src="image" alt="Product Image" width="100" height="100" class="aspect-square object-cover border border-zinc-200 w-full rounded-lg overflow-hidden dark:border-zinc-800">
+      <img id="main-img" :src="activeImage" alt="Product Image" width="600" height="600" class="aspect-square object-cover border border-zinc-200 w-full rounded-lg overflow-hidden dark:border-zinc-800"><div class="flex gap-4">
+        <img v-for="(image, index) in productData.images" :key="index" :src="image" alt="Product Image" width="100" height="100" class="aspect-square object-cover border border-zinc-200 w-full rounded-lg overflow-hidden dark:border-zinc-800" @click="setActiveImage(image)">
       </div>
     </div><div class="grid gap-4 md:gap-10 items-start">
       <h1 class="font-bold text-3xl">
@@ -23,7 +28,7 @@ const { handleIncrement } = useStore()
           Rating: {{ productData.rating }}
         </label><div class="text-sm"><p>{{ productData.category }}</p></div>
       </div>
-      <button class="bg-blue-600 hover:bg-blue-400 rounded-full p-2" @click="handleIncrement(productData)">Add to Cart</button>
+      <button class="bg-blue-600 hover:bg-blue-400 rounded-full p-2 text-white" @click="handleIncrement(productData)">Add to Cart</button>
     </div>
   </div>
 </template>
